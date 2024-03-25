ef1d914970d1da121248be480e577528119315b9
Rogério Soares <rgrsoares@yahoo.com.br>
2024-03-25 11:22:41 -0300

    Create the register user on backend
    Now the front will be created to display the form and test user registration.

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,14 +1,34 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema(
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
     {
-        name: String,
-        surname: String,
-        username: { type: String, unique: true },
-        email: { type: String, unique: true },
-        isActive: { type: Boolean, default: true },
+        name: { type: String, required: true },
+        surname: { type: String, required: true },
+        username: { type: String, unique: true, required: true },
+        email: { type: String, unique: true, required: true },
+        password: { type: String, minlength: 5, required: true },
+        isActive: { type: Boolean, default: true, required: true },
     },
     { timestamps: true }
 );
 
-export default mongoose.model('User', userSchema);
+// Criar hash da senha antes de salvar
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const saltRounds = parseInt(process.env.SALT_BCRYPT, 10); //Conversão para base 10
+        const salt = await bcrypt.genSalt(saltRounds);
+        this.password = await bcrypt.hash(this.password, salt);
+        return next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+const User = model('User', userSchema); // Nome do modelo
+
+export default User;
